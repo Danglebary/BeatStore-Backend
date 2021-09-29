f72a3bb85d6502173cf3be6024ed2a04676b7cdf
Danglebary <halfblowncontact@gmail.com>
2021-09-28 21:11:09 -0700

    added "MusicalKeys" type for createBeat input

--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,29 @@ export type MyContext = {
     res: Response;
     redis: Redis;
 };
+
+export type MusicalKeys =
+    | "c_major"
+    | "c_minor"
+    | "c_sharp_major"
+    | "c_sharp_minor"
+    | "d_major"
+    | "d_minor"
+    | "d_sharp_major"
+    | "d_sharp_minor"
+    | "e_major"
+    | "e_minor"
+    | "f_major"
+    | "f_minor"
+    | "f_sharp_major"
+    | "f_sharp_minor"
+    | "g_major"
+    | "g_minor"
+    | "g_sharp_major"
+    | "g_sharp_minor"
+    | "a_major"
+    | "a_minor"
+    | "a_sharp_major"
+    | "a_sharp_minor"
+    | "b_major"
+    | "b_minor";
