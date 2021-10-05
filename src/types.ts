b379952364d3597d127a97f062fad9f46fdfd02d
Danglebary <halfblowncontact@gmail.com>
2021-10-05 16:10:48 -0700

    changed musicalKeys from type to enum

--- src/types.ts
+++ src/types.ts
@@ -14,28 +14,29 @@ export type MyContext = {
     redis: Redis;
 };
 
-export type MusicalKeys =
-    | "c_major"
-    | "c_minor"
-    | "c_sharp_major"
-    | "c_sharp_minor"
-    | "d_major"
-    | "d_minor"
-    | "d_sharp_major"
-    | "d_sharp_minor"
-    | "e_major"
-    | "e_minor"
-    | "f_major"
-    | "f_minor"
-    | "f_sharp_major"
-    | "f_sharp_minor"
-    | "g_major"
-    | "g_minor"
-    | "g_sharp_major"
-    | "g_sharp_minor"
-    | "a_major"
-    | "a_minor"
-    | "a_sharp_major"
-    | "a_sharp_minor"
-    | "b_major"
-    | "b_minor";
+export enum MusicalKeys {
+    C_MAJOR = "c_major",
+    C_MINOR = "c_minor",
+    C_SHARP_MAJOR = "c_sharp_major",
+    C_SHARP_MINOR = "c_sharp_minor",
+    D_MAJOR = "d_major",
+    D_MINOR = "d_minor",
+    D_SHARP_MAJOR = "d_sharp_major",
+    D_SHARP_MINOR = "d_sharp_minor",
+    E_MAJOR = "e_major",
+    E_MINOR = "e_minor",
+    F_MAJOR = "f_major",
+    F_MINOR = "f_minor",
+    F_SHARP_MAJOR = "f_sharp_major",
+    F_SHARP_MINOR = "f_sharp_minor",
+    G_MAJOR = "g_major",
+    G_MINOR = "g_minor",
+    G_SHARP_MAJOR = "g_sharp_major",
+    G_SHARP_MINOR = "g_sharp_minor",
+    A_MAJOR = "a_major",
+    A_MINOR = "a_minor",
+    A_SHARP_MAJOR = "a_sharp_major",
+    A_SHARP_MINOR = "a_sharp_minor",
+    B_MAJOR = "b_major",
+    B_MINOR = "b_minor"
+}
