bc462079c743d2bfc0b497d036efc0f1f0422037
Danglebary <halfblowncontact@gmail.com>
2021-10-05 16:11:30 -0700

    added validateBeatUpdate, refactored

--- src/validation/validate_beat.ts
+++ src/validation/validate_beat.ts
@@ -1,6 +1,15 @@
 // Custom imports
-import { CreateBeatInput, FieldError, ValidationResponse } from "../orm_types";
-import { formatError, validateStringLength } from "./validation_base";
+import {
+    CreateBeatInput,
+    FieldError,
+    UpdateBeatInput,
+    ValidationResponse
+} from "../orm_types";
+import {
+    formatError,
+    validateStringLength,
+    validateMusicalKey
+} from "./validation_base";
 
 export const validateBeatUpload: (
     options: CreateBeatInput
@@ -8,17 +17,35 @@ export const validateBeatUpload: (
     const errors: FieldError[] = [];
 
     const isTitleValid = validateStringLength(options.title, 2, 100);
-    if (!isTitleValid) {
-        errors.push(formatError("title", "Too short"));
-    }
+    if (!isTitleValid) errors.push(formatError("title", "Too short"));
+
     if (options.genre) {
         const isGenreValid = validateStringLength(options.genre, 2, 100);
-        if (!isGenreValid) {
-            errors.push(formatError("genre", "Too short"));
-        }
+        if (!isGenreValid) errors.push(formatError("genre", "Too short"));
     }
 
     if (errors.length > 0) return { errors };
 
+    return { valid: true };
+};
+
+export const validateBeatUpdate: (
+    options: UpdateBeatInput
+) => ValidationResponse = (options) => {
+    const errors: FieldError[] = [];
+    if (options.title) {
+        const isTitleValid = validateStringLength(options.title, 2, 100);
+        if (!isTitleValid) errors.push(formatError("title", "Too short"));
+    }
+    if (options.genre) {
+        const isGenreValid = validateStringLength(options.genre, 2, 100);
+        if (!isGenreValid) errors.push(formatError("genre", "Too short"));
+    }
+    if (options.key) {
+        const isKeyValid = validateMusicalKey(options.key);
+        if (!isKeyValid) errors.push(formatError("key", "invalid value"));
+    }
+
+    if (errors.length > 0) return { errors };
     return { valid: true };
 };
