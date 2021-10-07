47ded8f4a6f97f98bf284f9db3e76111cd4afd44
Danglebary <halfblowncontact@gmail.com>
2021-10-06 17:33:48 -0700

    moved isAuth middleware to grpahql dir

--- src/middleware/graphQL/isAuth.ts
+++ src/middleware/graphQL/isAuth.ts
@@ -1,5 +1,7 @@
+// GraphQL imports
 import { MiddlewareFn } from "type-graphql";
-import { MyContext } from "../types";
+// Custom imports
+import { MyContext } from "../../types";
 
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
     if (!context.req.session.userId) {
