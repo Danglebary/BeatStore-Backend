f3a5d803115c393733cf99a3672f3cfa7cf3a1af
Danglebary <halfblowncontact@gmail.com>
2021-10-11 08:30:21 -0700

    swapped constants for env

--- src/middleware/server/sessionMiddleware.ts
+++ src/middleware/server/sessionMiddleware.ts
@@ -2,15 +2,13 @@
 import session from "express-session";
 import connectRedis from "connect-redis";
 import Redis from "ioredis";
-// Custom imports
-import { COOKIE_NAME, __prod__ } from "../../constants";
 
 export const sessionMiddleware = () => {
     const RedisStore = connectRedis(session);
     const redis = new Redis();
 
     const sessionMiddleware = session({
-        name: COOKIE_NAME,
+        name: process.env.COOKIE_NAME,
         store: new RedisStore({
             client: redis,
             disableTouch: true
@@ -20,7 +18,7 @@ export const sessionMiddleware = () => {
             maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
             httpOnly: true,
             sameSite: "lax",
-            secure: __prod__
+            secure: process.env.NODE_ENV === "production"
         },
         saveUninitialized: false,
         secret: "ha ha ha ha ha, you mad, or nah?",
