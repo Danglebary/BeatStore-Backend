dd6c31817dbe8eb39feca71348dd44038067bec8
Danglebary <halfblowncontact@gmail.com>
2021-10-12 18:01:20 -0700

    added s3 secret access key

--- src/module.d.ts
+++ src/module.d.ts
@@ -6,6 +6,6 @@ declare namespace NodeJS {
         S3_BUCKET_NAME: string;
         S3_REGION: string;
         S3_ACCESS_KEY: string;
-        S3_SECRET_ACCESS_KEY;
+        S3_SECRET_ACCESS_KEY: string;
     }
 }
